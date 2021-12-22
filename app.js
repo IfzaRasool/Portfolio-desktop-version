225cfc835ec2a3bea2aa0e7628bec8112b2e26e3
IfzaRasool <ifzarasoolarain@gmail.com>
2021-12-22 14:20:55 +0300

    Adjust humburger menu
    -Add container
    -Add Grid container and make some styles

--- app.js
+++ app.js
@@ -10,15 +10,27 @@ const menuItems = document.querySelectorAll('.menuItem');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
+const menuArrow1 = document.querySelector('.menu-arrow1');
+const menuArrow2 = document.querySelector('.menu-arrow2');
+const menuArrow3 = document.querySelector('.menu-arrow3');
 closeIcon.style.display = 'none';
+menuArrow1.style.display = 'none';
+menuArrow2.style.display = 'none';
+menuArrow3.style.display = 'none';
 function toggleMenu() {
   if (menu.classList.contains('showMenu')) {
     menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
+    menuArrow1.style.display = 'none';
+    menuArrow2.style.display = 'none';
+    menuArrow3.style.display = 'none';
     menuIcon.style.display = 'block';
   } else {
     menu.classList.add('showMenu');
     closeIcon.style.display = 'block';
+    menuArrow1.style.display = 'block';
+    menuArrow2.style.display = 'block';
+    menuArrow3.style.display = 'block';
     menuIcon.style.display = 'none';
   }
 }
